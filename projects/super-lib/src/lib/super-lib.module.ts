2813ef4c7f8b44aa6ef2ca22fd318ad2d3dd8a2f
DanaDov <danadovzh16@gmail.com>
2023-04-07 13:05:46 +0300

    initial

---
+++ projects/super-lib/src/lib/super-lib.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { SuperLibComponent } from './super-lib.component';
+
+
+
+@NgModule({
+  declarations: [
+    SuperLibComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    SuperLibComponent
+  ]
+})
+export class SuperUiLibModule { }
---
+++ projects/super-lib/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of super-lib
+ */
+
+export * from './lib/super-lib.service';
+export * from './lib/super-lib.component';
+export * from './lib/super-lib.module';
